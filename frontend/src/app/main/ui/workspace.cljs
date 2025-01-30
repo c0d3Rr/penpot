b3f748778f603088851a2a60762bfb65dbf0f45b
Kaleidos Ventures <kaleidosventures@MacBook-Pro-de-Kaleidos.local>
2025-01-30 07:26:42 +0100

    Adds tips to the file loading screen

--- frontend/src/app/main/ui/workspace.cljs
+++ frontend/src/app/main/ui/workspace.cljs
@@ -128,7 +128,8 @@
   []
   [:> loader*  {:title (tr "labels.loading")
                 :class (stl/css :workspace-loader)
-                :overlay true}])
+                :overlay true
+                :file-loading true}])
 
 (mf/defc workspace-page
   {::mf/wrap-props false}
